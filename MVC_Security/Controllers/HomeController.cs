ab76791904bea4e881d83515859b4dea5ea124f2
Yogesh Belwal <65054623+Yogesh-Belwal@users.noreply.github.com>
2021-07-05 11:32:00 +0530

    Files Upload

---
+++ MVC_Security/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using MVC_Security.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC_Security.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize (Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(tblLogin model, string returnUrl)
+        {
+            DemoDBEntities db = new DemoDBEntities();
+            var dataItem = db.tblLogins.Where(x => x.Username == model.Username && x.Password == model.Password).First();
+            if (dataItem != null)
+            {
+                FormsAuthentication.SetAuthCookie(dataItem.Username, false);
+                if(Url.IsLocalUrl(returnUrl) && returnUrl.Length >1 && returnUrl.StartsWith("/")
+                    && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid user/pass");
+                return View();
+            }
+        }
+        [Authorize]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
